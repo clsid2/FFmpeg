233207c18505551682d80bf6e1b3b5b720abd4f0
Hendrik Leppkes <h.leppkes@gmail.com>
2013-04-06 18:04:50 +0200

    matroskadec_haali: added an option to suppress opening external segments

--- libavformat/avformat.h
+++ libavformat/avformat.h
@@ -1436,6 +1436,7 @@ typedef struct AVFormatContext {
 #define AVFMT_FLAG_FAST_SEEK   0x80000 ///< Enable fast, but inaccurate seeks for some formats
 #define AVFMT_FLAG_AUTO_BSF   0x200000 ///< Add bitstream filters as requested by the muxer
 
+#define AVFMT_FLAG_NOEXTERNAL     0x40000000 ///< Do not open external files referenced by the format
 #define AVFMT_FLAG_NETWORK        0x80000000 ///< Source is a network protocol, optimize for that
 
     /**
