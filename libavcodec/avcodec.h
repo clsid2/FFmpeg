e31c207f66ab83ba01284a6a3a48429a17a8af43
Hendrik Leppkes <h.leppkes@gmail.com>
2013-06-16 20:30:33 +0200

    HACK: avcodec: export progressive_sequence information

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -2024,6 +2024,13 @@ typedef struct AVCodecContext {
      * - decoding: unused
      */
     int (*get_encode_buffer)(struct AVCodecContext *s, AVPacket *pkt, int flags);
+
+    /**
+     * Is the stream completely progressive?
+     * - decoding: set by avcodec
+     * - encoding: unused
+     */
+    int progressive_sequence;
 } AVCodecContext;
 
 struct MpegEncContext;
