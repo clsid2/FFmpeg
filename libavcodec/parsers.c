55fa5d87050debaa0fe599f6e3d5388cdca099c4
Hendrik Leppkes <h.leppkes@gmail.com>
2016-01-09 16:34:40 +0100

    h264_parser: add support for parsing h264 mvc NALUs

--- libavcodec/parsers.c
+++ libavcodec/parsers.c
@@ -49,6 +49,7 @@ extern const AVCodecParser ff_gsm_parser;
 extern const AVCodecParser ff_h261_parser;
 extern const AVCodecParser ff_h263_parser;
 extern const AVCodecParser ff_h264_parser;
+extern const AVCodecParser ff_h264_mvc_parser;
 extern const AVCodecParser ff_hevc_parser;
 extern const AVCodecParser ff_ipu_parser;
 extern const AVCodecParser ff_jpeg2000_parser;
