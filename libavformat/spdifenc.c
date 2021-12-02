15cd9402dc814e8d8752431b4677aaac87179260
Hendrik Leppkes <h.leppkes@gmail.com>
2021-12-02 10:55:51 +0100

    spdifenc: mark as NOFILE

--- libavformat/spdifenc.c
+++ libavformat/spdifenc.c
@@ -683,6 +683,6 @@ const AVOutputFormat ff_spdif_muxer = {
     .write_header      = spdif_write_header,
     .write_packet      = spdif_write_packet,
     .deinit            = spdif_deinit,
-    .flags             = AVFMT_NOTIMESTAMPS,
+    .flags             = AVFMT_NOTIMESTAMPS | AVFMT_NOFILE,
     .priv_class        = &spdif_class,
 };
